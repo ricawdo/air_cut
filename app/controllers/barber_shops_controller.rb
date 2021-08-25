9f3b6633aa010a71981de47b465d128ebc2af303
Bruno Goncalves <bruno.goncalves.1996@gmail.com>
2021-08-25 11:20:45 +0200

    ok

--- app/controllers/barber_shops_controller.rb
+++ app/controllers/barber_shops_controller.rb
@@ -1,12 +1,16 @@
 class BarberShopsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
+  skip_before_action :authenticate_user!, only: [ :index, :search ]
 
   def index
     @barber_shops = policy_scope(BarberShop)
+  end
+  
+  def search
     if params[:query].present?
       @barber_shops = BarberShop.where(address: params[:query])
     else
       @barber_shops = BarberShop.all
     end
+    authorize @barber_shops
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :barber_shops, only: [:index]
+  get "/search", to: 'barber_shops#search'
 end
