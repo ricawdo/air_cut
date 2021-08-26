e8e37b477fa24c2e114789f0e1bac7ea14be9534
Ricardo Dupot <ricawdo@gmail.com>
2021-08-26 12:36:58 +0200

    add mapbox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,6 +25,7 @@ require("channels")
 // External imports
 import "bootstrap";
 import { initFlatpickr } from "../plugins/flatpickr";
+import { initMapbox } from '../plugins/init_mapbox';
 
 
 // Internal imports, e.g:
@@ -33,4 +34,8 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   // initFlatpickr();
+  const mapElement = document.getElementById('map');
+  if (mapElement) {
+    initMapbox();
+  }
 });
