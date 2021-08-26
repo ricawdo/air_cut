e365f13ab7aaf104e5a29da032dee1052166625f
Bruno Goncalves <bruno.goncalves.1996@gmail.com>
2021-08-26 15:50:07 +0200

    resolve problem recherche

--- app/controllers/barber_shops_controller.rb
+++ app/controllers/barber_shops_controller.rb
@@ -13,10 +13,10 @@ class BarberShopsController < ApplicationController
       @services = Service.select {|service| service.gender == params[:gender] && service.name == params[:service] }
       @shop_services = @services.map { |service| ShopService.select { |shop_service| shop_service.service == service } }.flatten
       @barber_shops = @shop_services.map { |shop_service| BarberShop.find(shop_service.barber_shop_id) }.uniq
-      # raise
+      @barber_shops = BarberShop.where(id: @barber_shops.map(&:id))
       render :index
     end
-    authorize @barber_shops.first
+    skip_authorization
     @markers = @barber_shops.geocoded.map do |barber_shop|
       { lat: barber_shop.latitude, lng: barber_shop.longitude, info_window: render_to_string(partial: "info_window", locals: { barber_shop: barber_shop }) }
     end
