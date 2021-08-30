05b9c6f3234b68b42109bd75603ab58ceb8ed96c
Bruno Goncalves <bruno.goncalves.1996@gmail.com>
2021-08-30 12:10:05 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/search", to: 'barber_shops#search'
   get "/profile", to: 'pages#profile'
+  get "/favorie", to: 'pages#favorie'
   get "/users/:id/edit", to: 'pages#edit', as: 'user_edit'
   patch "/users/:id", to: 'pages#update'
 
